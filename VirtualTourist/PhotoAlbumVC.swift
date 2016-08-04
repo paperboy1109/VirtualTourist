a770da3f7b36c68da1eb258fa3d1a3f58f02bcc7
Daniel Janiak <danieljjaniak@icloud.com>
2016-08-04 11:32:17 -0700

    Add delegate methods for the collection view

--- VirtualTourist/PhotoAlbumVC.swift
+++ VirtualTourist/PhotoAlbumVC.swift
@@ -13,6 +13,8 @@ class PhotoAlbumVC: UIViewController {
     
     // MARK: - Properties
     
+    var sharedContext = CoreDataStack.sharedInstance().managedObjectContext
+    
     var mapAnnotation: CustomPinAnnotation!
     
     // MARK: - Outlets
@@ -20,9 +22,12 @@ class PhotoAlbumVC: UIViewController {
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var collectionView: UICollectionView!
     
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        /* Configure the map */
         var mapRegion = MKCoordinateRegion()
         var mapSpan = MKCoordinateSpan()
         mapSpan.latitudeDelta = 0.02
@@ -39,6 +44,10 @@ class PhotoAlbumVC: UIViewController {
         
         mapView.addAnnotation(mapAnnotation)
         
+        /* Configure the collection view */
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -50,4 +59,63 @@ class PhotoAlbumVC: UIViewController {
         print(self.mapAnnotation.pin)
     }
     
+    // MARK: - Helpers
+    
 }
+
+
+// MARK: - Delegate methods for the collection view
+
+extension PhotoAlbumVC: UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        
+        // Filler code
+        return 1
+        
+        // TODO: Implement the fetched results controller
+        
+        // Use the Fetched Results Controller
+        //print("in numberOfSectionsInCollectionView()")
+        //return self.fetchedResultsController.sections!.count ?? 0
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // Filler code
+        return 1
+        
+        // TODO: Implement the fetched results controller
+        // Use the Fetched Results Controller
+        /*
+        print("in collectionView(_:numberOfItemsInSection)")
+        let sectionInfo = fetchedResultsController.sections![section]
+        print("number Of Cells: \(sectionInfo.numberOfObjects)")
+        return sectionInfo.numberOfObjects */
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        print("in collectionView(_:cellForItemAtIndexPath)")
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("TouristPhotoCell", forIndexPath: indexPath) as UICollectionViewCell
+        
+        // let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as! DeleteCellsCollectionViewCell
+        // cell.imageView.backgroundColor = UIColor.blueColor()
+        
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        print("in collectionView(_:didSelectItemAtIndexPath)")
+        
+        print("Cell at index path \(indexPath) was tapped ")
+
+        
+    }
+    
+}
+
+
